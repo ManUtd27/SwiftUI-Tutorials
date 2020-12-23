c4a620df55357cc842040d5a01b524634b0d32df
Shawn Williams <jr.william09@gmail.com>
2020-12-23 11:14:35 -0600

    Created a basic navigation list with a detail view

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,59 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Shawn Williams on 12/23/20.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    
+    var landmark: Landmark
+    
+    
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.primary)
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 5.0) {
+                        Text("About \(landmark.name)")
+                            .font(.title2)
+                        Text(landmark.description)
+                    }
+                }
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+        .statusBar(hidden: true)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}
